ccdebe3abd5e679871bd674ea8b90f90845981b8
FabioMartins <fabbio1000@gmail.com>
2022-04-26 17:24:28 -0300

    requisito 2

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div data-testid="home-initial-message">
+        <h2>Digite algum termo de pesquisa ou escolha uma categoria.</h2>
+      </div>
+    );
+  }
+}
+
+export default Home;
