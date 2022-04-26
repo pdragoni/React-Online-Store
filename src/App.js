efac4775ac4232af1d88a374af65becc972f1d53
paulo_dragoni <paulohbfdf@gmail.com>
2022-04-26 18:14:20 -0300

    Requisito 3 - finalizado. Implementação de carrinho de compras vazio.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import Home from './components/Home';
+import Cart from './components/Cart';
 
 class App extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class App extends React.Component {
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={ Home } />
+            <Route exact path="/cart" component={ Cart } />
           </Switch>
         </BrowserRouter>
       </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   render() {
     return (
       <div data-testid="home-initial-message">
-        <h2>Digite algum termo de pesquisa ou escolha uma categoria.</h2>
+        <Link
+          id="button"
+          data-testid="shopping-cart-button"
+          to="/cart"
+        >
+          Carrinho
+        </Link>
+        <label htmlFor="button">
+          <input type="text" placeholder="Insira sua busca" />
+          <button type="button">Botão</button>
+        </label>
+        <h4>Digite algum termo de pesquisa ou escolha uma categoria.</h4>
       </div>
     );
   }
