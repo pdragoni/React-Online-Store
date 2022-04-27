e0b942aa979115d04df17c3948d1822dfb172f10
paulo_dragoni <paulohbfdf@gmail.com>
2022-04-27 18:29:20 -0300

    Princípios requisito 6

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { getCategories } from '../services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
+import '../index.css';
 
 class Home extends React.Component {
   constructor() {
@@ -8,6 +9,7 @@ class Home extends React.Component {
 
     this.state = {
       categorias: [],
+      produtos: [],
     };
   }
 
@@ -19,11 +21,20 @@ class Home extends React.Component {
     });
   }
 
+  byCategory = async (id) => { // pego o alvo do clique
+    const products = await getProductsFromCategoryAndQuery(id);
+    console.log(products.results);
+    this.setState({
+      produtos: products,
+    });
+  }
+
   render() {
-    const { categorias } = this.state;
+    const { categorias, produtos } = this.state;
     return (
       <div data-testid="home-initial-message">
         <Link
+          className="carrinho"
           data-testid="shopping-cart-button"
           to="/cart"
         >
@@ -31,22 +42,35 @@ class Home extends React.Component {
         </Link>
         <br />
         <br />
-
-        <label htmlFor="button">
-          <h4>Digite algum termo de pesquisa ou escolha uma categoria.</h4>
-          <input id="button" type="text" placeholder="Insira sua busca" />
+        <h4
+          className="digite"
+        >
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h4>
+        <label className="homeLabel" htmlFor="button">
+          <input
+            id="button"
+            type="text"
+            placeholder="Insira sua busca"
+            className="pesquisa"
+          />
           <button type="button">Botão</button>
         </label>
         <br />
         <br />
         <section>
+          { (produtos.length === 0 ? <h4>Nenhum resultado</h4> : <p>Tem algo</p>) }
+        </section>
+        <section className="categorias">
           {
             categorias.length > 0
               ? (categorias.map((cat) => (
                 <button
+                  className="category"
                   key={ cat.id }
                   type="button"
                   data-testid="category"
+                  onClick={ () => this.byCategory(cat.id) }
                 >
                   { cat.name }
                 </button>
